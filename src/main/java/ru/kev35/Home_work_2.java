fff83cf32da8488c85c570d7fdd7cbf53df62928
Kuznetsov_Evgeny <kev35.vol@gmail.com>
2025-10-04 12:31:25 +0300

    init commit

---
+++ src/main/java/ru/kev35/Home_work_2.java
@@ -0,0 +1,13 @@
+package ru.kev35;
+
+public class Home_work_2 {
+    public static void main(String[] args) {
+
+        Home_work_2 hello = new Home_work_2();
+        hello.Hello();
+
+    }
+    public void Hello(){
+        System.out.print("Создаём конфликт!");
+    }
+}
